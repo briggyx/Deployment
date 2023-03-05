4c77c258aee0e772d5ef62a36872015f31d631e5
Brigitte Yan <92674461+briggyx@users.noreply.github.com>
2023-03-04 16:16:24 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,148 @@
+# Deloyment Project 
+
+### [Assignment](assignment.md)
+
+## Project/Goals
+
+**The goal** of this project is to use machine learning to automate the process of determining loan eligibility based on customer information collected through online application forms. The dataset contains details pertaining to the customer's demographic and socioeconomic attributes. 
+
+**The aim** is to predict whether a loan will be approved or not, based on the provided customer information. 
+
+The dataset, which includes variables such as Loan_ID, Gender, Married, Dependents, Education, Self_Employed, ApplicantIncome, CoapplicantIncome, LoanAmount, Loan_Amount_Term, Credit_History, Property_Area, and Loan_Status, can be found [here](https://drive.google.com/file/d/1h_jl9xqqqHflI5PsuiQd_soNYxzFfjKw/view?usp=sharing).
+
+## Hypothesis
+
+The initial phase of data analysis involves generating a hypothesis, which requires a clear understanding of the problem and the development of a relevant hypothesis that could positively impact the outcome. 
+It is important to create this hypothesis prior to deeply examining the data, so that we can create a list of various analyses that may be performed once data is available.
+
+Brief research on factors involved in determining the approval of loans yields to the following key points:
+- Lenders consider **credit scores and reports** as they indicate how well individuals manage borrowed money and a poor credit history raises the risk of default. [Source](https://www.fool.com/the-ascent/personal-loans/articles/7-factors-lenders-look-considering-your-loan-application/)
+- To ensure that borrowers can repay their loans, lenders assess their **income level and stability**, with higher income requirements for larger loans. In addition, steady employment is necessary, making it more difficult for self-employed individuals or those who only work part of the year to obtain a loan. [Source](https://www.fool.com/the-ascent/personal-loans/articles/7-factors-lenders-look-considering-your-loan-application/)
+- Lenders assess a borrower's ability to make payments on current and new debt by calculating their **Debt-to-Income (DTI) ratio**, which is the percentage of their monthly gross income that goes towards debt service.[Source](https://www.forbes.com/advisor/personal-loans/personal-loan-requirements/)
+- When applying for a secured personal loan, lenders typically require **collateral**, such as assets related to the loan's purpose or other valuable items like investments or collectibles, which they can seize in case of default or missed payments to recover the remaining loan amount. [Source](https://www.forbes.com/advisor/personal-loans/personal-loan-requirements/)
+- Lenders evaluate loan applications by taking into account **economic conditions**, industry trends, and upcoming legislation that affect the borrower's business. [Source](https://www.bankofamerica.com/smallbusiness/resources/post/factors-that-impact-loan-decisions-and-how-to-increase-your-approval-odds/)
+
+Based on prior research on the domain and the dataset's attributes, several hypothesis for predicting loan approval for applicants:
+- High income relative to loan
+- Presence of coapplicant (with relatively high income)
+- Married
+- Low number of dependents 
+- Graduate
+- Not self-employed
+- Urban/semi-urban property
+- Credit history present 
+
+It may be worth noting that the dataset *does not specify* the type of loan that's requested for, such as personal,  mortgage, home-equity, etc. Other possiblity important information that's missing includes a person's existing debts and assets, and indication of whether the income is net or gross. 
+These attributes may affect loan approval/disapproval, and without it, our predictions may not be as accurate as they could otherwise be. 
+
+## EDA 
+**CreditHistory** has the strongest positive correlation with LoanApproval (0.567105).
+**PropertyArea_rural** has the most negative correlation wrt LoanApproval (-0.109203).
+
+Negative correlations wrt Loan Approval:
+- LoanAmount               -0.018536
+- LoanAmountTerm           -0.033173
+- SelfEmployed             -0.005660 (closest to 0)
+- Gender_Female            -0.055320
+- Gender_Unknown           -0.074998
+- Married_No               -0.083304
+- Dependents_0             -0.011764
+- Dependents_1             -0.037075
+- PropertyArea_Rural       -0.109203 (most negative)
+- PropertyArea_Urban       -0.027789
+
+Positive correlations wrt Loan Approval:
+- ApplicantIncome           0.006396 
+- CoapplicantIncome         0.066459
+- Graduate                  0.099686
+- CreditHistory             0.567105 (most positive)
+- Gender_Male               0.077791
+- Married_Unknown           0.040590
+- Married_Yes               0.077816
+- Dependents_2              0.047410
+- Dependents_3              0.005173 (closest to 0)
+- PropertyArea_Semiurban    0.129037
+
+People who are approved for loans are most likely to: have coapplicants with high income, be a graduate, have a credit history, be male, be married/unknown, have two children and live in the suburbs.
+
+Those who aren't approved tend to: ask for a large loan and long terms, are female/unknown gender, aren't married, have none or 1 child, and live in a rural or urban area. 
+
+Interestingly, ApplicantIncome has only a very weak positive correlation with LoanApproval.
+
+Features with a p-value < 5 with LoanApproval are Graduate (0.023) and PropertyArea_Semiurban (0.003), CreditHistory (0.000) and PropertyArea_Rural (0.013).
+This implies that their correlations with LoanApproval is statistically significant.
+
+Going back to my list of hypothesis of who gets loan approvals, I can accept/reject some of them based on the information I have currently:
+- Applicants with high income in comparison to the loan amount UNSURE 
+- Having a co-applicant with a relatively high income UNSURE 
+- Being married UNSURE
+- Having a small number of dependents UNSURE
+- **Being a graduate ACCEPT**
+- Not being self-employed UNSURE 
+- **Owning a property in a semi-urban area ACCEPT**
+- **Having a credit history ACCEPT**
+
+## Process
+**Process steps overview:**
+1. Hypothesis Generation 
+2. Data Cleaning
+3. Exploratory Data Analysis  
+4. Feature Engineering & Model Building 
+5. Pipeline
+6. API
+7. Deployment 
+
+### 1. Hypothesis Generation 
+- Briefly research factors that influence loan approval. 
+
+- Look at info available in dataset. 
+
+- List of educated guesses/hypothesis about factors that influence loan approval in the context of the dataset.
+
+
+### 2. Data Cleaning
+- Impute NaNs with mode or median 
+- OHE categorical features 
+- Create 3rd ‘Unknown’ category for marriage and gender instead of imputing F/M or M/not-M, to acknowledge more possibilities for those
+- Remove outliers 
+- Try regression to impute NaNs
+
+### 3. Exploratory Data Analysis
+- Correlations between LoanStatus and other features
+- Pearson’s test
+    -  Significant positive correlations between loan approval and ‘graduated’, suburban, credit history present 
+    - Significant negative correlation with living in rural area
+- Tentatively accept/reject hypothesis from earlier list 
+
+### 4. Feature Engineering & Model Building 
+- MinMax scale, PCA 
+- Try k-folds cv with various models, use model with highest av. accuracy
+- Grid search on logistic regression model to find best hyperparameters 
+- estimator = grid.best_estimator_ ; pickle model
+
+### 5. Pipeline 
+- **LoanData** class contains methods for:
+    -  loading a CSV
+    - cleaning and scaling
+    - Returning X and y for model fitting or prediction
+- **PredictionPipeline** class contains methods for:
+    - Testing and fitting a logistic regression model
+    - Making predictions using the model
+
+### 6. API
+- An app was created, however, technical difficulties were encountered and I was unable to finish implementing it 
+### 7. Deployment 
+- Unfinished 
+
+
+## Results/Demo
+Best parameters from grid search for logistic regression using PC's:
+- 'lr__C': 1, 'lr__penalty': 'l1', 'lr__solver': 'liblinear', 'pca__n_components': 15
+- Accuracy score: 0.8279226598557153
+
+## Challanges 
+- technical difficulties in implementing an API & deployment 
+
+## Future Goals
+- finish creating an API & deploying to the cloud 
+- create a website with polished UI & UX 
---
+++ src/APP_no_nn/loans.py
@@ -0,0 +1,243 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import collections
+from scipy import stats
+from sklearn.linear_model import LinearRegression
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler
+from scipy import stats
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.decomposition import PCA
+from sklearn.metrics import f1_score, mean_absolute_error, mean_absolute_percentage_error, mean_squared_error
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score, KFold, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+import pickle as pk
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+
+class LoanData:
+    
+    def __init__(self,csv_path,do_cleaning=True):
+        """Initialize a new LoanPipeline, loading data from the specified CSV file"""
+        self.original = pd.read_csv(csv_path)
+        if do_cleaning:
+            self.clean()
+        else:
+            self.cleaned = None
+
+    def __repr__(self):
+        """Equivalent to the most complete internal dataframe's __repr__"""
+
+        if self.cleaned is not None:
+            print('CLEANED DATA:')
+            display(self.cleaned)
+        else:
+            print('ORIGINAL DATA:')
+            display(self.original)
+        return('')
+        
+    def to_csv(self,csv_path,which='original'):
+        """Write the data to a CSV; which=['original','cleaned']"""
+        if which == 'original':
+            self.original.to_csv(csv_path,index=False)
+        elif which == 'cleaned':
+            if self.cleaned is None:
+                print('Error: no cleaned data to write to CSV.')
+                return
+            self.cleaned.to_csv(csv_path,index=False)
+        else:
+            print(f'Invalid value for which parameter: {which}')
+    
+    def clean(self):
+        """Tidy column names, fill in missing data, convert data types"""
+        
+        # Remove trailing whitespace from strings
+        self.original = self.original.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+        
+        # Remove underscores from column names for consistency
+        self.original = self.original.rename(columns=lambda x: x.replace('_', ''))
+        
+        # Make index = loan ID, and remove loan ID column
+        self.cleaned = self.original.copy()
+        self.cleaned.index = self.cleaned.LoanID
+        self.cleaned = self.cleaned[self.cleaned.columns[1:]]
+        
+        # Fill missing data with median, mode, or zero, as appropriate
+        medians = self.cleaned[['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'LoanAmountTerm']].median() # Calculate medians
+        modes = self.cleaned[['Dependents','SelfEmployed']].mode() # Calculate modes
+        replace = { x:medians[x] for x in medians.index } # Dict to hold column name to value to sub in place of nan mapping
+        replace['Dependents'] = modes.Dependents.sample(1)[0] # Sample in case mode returned multiple values
+        replace['SelfEmployed'] = modes.SelfEmployed.sample(1)[0] # Sample in case mode returned multiple values
+        replace['CreditHistory'] = 0 # Assume missing values in CreditHistory mean no history is available, = 0
+        replace['Gender'] = 'Unknown'
+        replace['Married'] = 'Unknown'
+        print('FILLING IN MISSING DATA AS FOLLOWS:')
+        for col,val in replace.items():
+            print(f'Missing data in {col} replaced with {val}')
+        self.cleaned.fillna(replace,inplace=True) # Fill nans with medians
+        
+        # Convert numeric data to float32; some would be fine as integers due to lack of decimals but new data may break that assumption
+        self.cleaned.ApplicantIncome = self.cleaned.ApplicantIncome.astype(np.float32)
+        self.cleaned.CoapplicantIncome = self.cleaned.CoapplicantIncome.astype(np.float32)
+        self.cleaned.LoanAmount = self.cleaned.LoanAmount.astype(np.float32)
+        self.cleaned.LoanAmountTerm = self.cleaned.LoanAmountTerm.astype(np.float32)
+        
+        # Convert ordered categorical data to unsigned 8-bit integers
+        self.cleaned.Dependents = self.cleaned.Dependents.map({'0':0,'1':1,'2':2,'3+':3}).astype(np.uint8)
+        self.cleaned.SelfEmployed = self.cleaned.SelfEmployed.map({'Yes': 1, 'No': 0}).astype(np.uint8)
+        self.cleaned.LoanStatus = self.cleaned.LoanStatus.map({'Y': 1, 'N': 0}).astype(np.uint8)
+        self.cleaned.Education = self.cleaned.Education.map({'Graduate': 1, 'Not Graduate': 0}).astype(np.uint8)
+        self.cleaned.rename(columns={'Education':'Graduate'},inplace=True)
+        
+        # One-hot-encode gender and marriage status (necessary because missing data was treated as 'other')
+        gender = pd.get_dummies(self.cleaned.Gender)
+        marriage = pd.get_dummies(self.cleaned.Married)
+        gender.columns = [ f'Gender_{g}' for g in gender.columns ]
+        marriage.columns = [ f'Married_{m}' for m in marriage.columns ]
+        prop = pd.get_dummies(self.cleaned.PropertyArea)
+        one_hot = pd.merge(pd.merge(gender,marriage,left_index=True,right_index=True),prop,left_index=True,right_index=True)
+        
+        # Merge everything
+        self.cleaned = pd.merge(one_hot,self.cleaned[self.cleaned.columns[2:].drop('PropertyArea')],left_index=True,right_index=True)
+    
+        # Remove outliers
+        print('\nREMOVING OUTLIERS:')
+        cols_to_check = ['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount']
+        
+        for col in cols_to_check:
+            print(f"Initial range of {col}: {self.cleaned[col].min()} - {self.cleaned[col].max()}")
+        
+        q1, q4 = self.cleaned[cols_to_check].quantile(0.25), self.cleaned[cols_to_check].quantile(0.75)
+        iqr = q4 - q1
+        lower_bound = q1 - 1.5 * iqr
+        upper_bound = q4 + 1.5 * iqr
+        
+        mask = (self.cleaned[cols_to_check] >= lower_bound) & (self.cleaned[cols_to_check] <= upper_bound)
+        self.cleaned = self.cleaned[mask.all(axis=1)].copy()
+        
+        for col in cols_to_check:
+            print(f"Filtered range of {col}: {self.cleaned[col].min()} - {self.cleaned[col].max()}")
+
+class PredictionPipeline:
+    """Implements data cleaning and loan prediction functionality; initialize with a path to a CSV file"""
+    
+    def __init__(self):
+        self.scaler = None
+        self.estimator = None
+    
+    def _check_data_(self,data):
+        if type(data) == LoanData:
+            if data.cleaned is not None:
+                data = data.cleaned
+            else:
+                print('Error: LoanData object passed to fit_scaler, but no cleaned data found; run LoanData.clean() first')
+                return
+        return data
+    
+    def _get_Xy_(self,data,label_target):
+        data = pd.DataFrame(self.scaler.transform(data),columns=data.columns)
+        X = data.drop(columns=[label_target])
+        y = data[label_target] if label_target in data.columns else None
+        return X,y
+    
+    def fit_scaler(self,data,scaler = MinMaxScaler()):
+        """Fit a scaler, default = min-max"""
+        
+        data = self._check_data_(data)
+        scaler.fit_transform(data)
+        self.scaler = scaler
+
+    def test_and_fit_estimator(self,data,
+                               estimator=LogisticRegression(),
+                               label_target='LoanStatus',
+                               ncomp=np.arange(2,20,1),
+                               penalties=["l1", "l2"],
+                               C=np.arange(1,25,2.5)/5,
+                               solvers=['liblinear']):
+        """Takes an estimator and uses grid search to find the best parameters, and reports these along with model accuracy, prompting the user to accept or reject the model"""
+        
+        data = self._check_data_(data)
+        
+        if self.scaler is None:
+            print('Error: No scaler implemented yet; run PredictionPipeline.fit_scaler() first.')
+            return
+        
+        X,y = self._get_Xy_(data,label_target)
+        
+        param_grid = { "pca__n_components": ncomp, "lr__penalty": penalties,
+        "lr__C": C, "lr__solver": solvers }
+
+        # Combine PCA and logistic regression into a pipeline
+        pipe = Pipeline([("pca", PCA()),("lr", estimator)])
+
+        # Create a grid search object
+        grid = GridSearchCV(pipe, param_grid, cv=3)
+        
+        # Fit the grid search object to the data
+        grid.fit(X, y)
+
+        # Print the best hyperparameters and accuracy score
+        print("Best hyperparameters:", grid.best_params_)
+        print("Accuracy score:", grid.best_score_)
+        
+        # Prompt the user to accept or reject the model
+        accept = input('Do you wish to implement this model? (y/n):')
+        if accept.upper() != 'Y':
+            print('Model not implemented; please run PredictionPipeline.test_and_fit() again')
+            return
+        
+        # If the model is good, fit and store it
+        self.estimator = grid.best_estimator_
+        self.estimator.fit(X,y)
+        print('Model has been fit and implemented.')
+    
+    def predict(self,data,label_target='LoanStatus'):
+        """Run predictions; if y is supplied, accuracy is checked"""
+        
+        data = self._check_data_(data)
+        
+        if self.scaler is None:
+            print('Error: No scaler implemented yet; run PredictionPipeline.fit_scaler() first.')
+            return
+        
+        X,y = self._get_Xy_(data,label_target)
+            
+        if self.estimator is None:
+            print('Error, no estimator is implemented; run PredictionPipeline.test_and_fit() first.')
+            return
+
+        predictions = self.estimator.predict(X)
+            
+        if y is not None:
+                
+            predictions_0 = predictions[y==0]
+            predictions_1 = predictions[y==1]
+
+            y_0 = y[y==0]
+            y_1 = y[y==1]
+
+            accuracy_total = sum(predictions == y)/len(predictions)
+            accuracy_0 = sum(predictions_0 == y_0)/len(predictions_0)
+            accuracy_1 = sum(predictions_1 == y_1)/len(predictions_1)
+            
+            print(f'Accuracy (overall): {round(accuracy_total*100,2)}%')
+            print(f'Accuracy (y=0): {round(accuracy_0*100,2)}%')
+            print(f'Accuracy (y=1): {round(accuracy_1*100,2)}%')
+            
+        return predictions
+    
+    def save(self,prefix):
+        pk.dump(self.scaler,open(f'{prefix}.scaler.pk','wb'))
+        pk.dump(self.estimator,open(f'{prefix}.estimator.pk','wb'))
+        
+    def load(self,prefix):
+        self.scaler = pk.load(open(f'{prefix}.scaler.pk','rb'))
+        self.estimator = pk.load(open(f'{prefix}.estimator.pk','rb'))
